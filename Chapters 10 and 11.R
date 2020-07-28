71965daead93217bb89f4ac7a79c4d3d0a0f7a17
mklomo <mklomo@icloud.com>
2020-07-28 23:13:20 +0000

    Chapters 10 and 11 under review

---
+++ Chapters 10 and 11.R
@@ -0,0 +1,70 @@
+#Chapter 10: Mastering the Grammar
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+#Building a Scatterplot
+ggplot(data = mpg, aes(x = displ, y = hwy, colour = factor(cyl))) +
+  geom_point(size = 2)
+
+new_df <- data.frame(x = c(1.8, 1.8, 2.0, 2.0, 2.8, 2.8, 3.1, 1.8),
+                     y = c(29, 29, 31, 30, 26, 26, 27, 26),
+                     colour = c(4, 4, 4, 4, 6, 6, 6, 4))
+ggplot(data = mpg, aes(x = displ, y = hwy, colour = factor(cyl))) +
+  geom_line() +
+  theme(legend.position = "none")
+
+
+#Overlaying multiple geoms
+ggplot(data = mpg, aes(x = displ, y = hwy, colour = cyl)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+#Note the effect of factor
+ggplot(data = mpg, aes(x = displ, y = hwy, colour = factor(cyl))) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
+#10.3 Adding complexity
+
+ggplot(data = mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~year)
+
+#11 Build a plot layer by layer
+#Each layer can come from a different dataset and have a different aesthetic
+#mapping, making it possible to create sophisticated plots that display data
+#from multiple sources
+p <- ggplot(data = mpg, aes(x = displ, y = hwy))
+
+#Addign a layer
+p +geom_point()
+
+data(mpg)
+mod <- loess(hwy ~ displ, data = mpg)
+grid <- tibble(seq(from = min(mpg$displ), to = max(mpg$displ), length.out = 50))
+names(grid) <- c("displ")
+grid$hwy <- predict(mod, newdata = grid)
+
+
+mpg$std_resid <- resid(mod)/mod$s
+outlier <- mpg %>%
+  subset(abs(std_resid) > 2)
+
+p + geom_point() +
+  geom_line(data = grid, aes( colour = "red", size = 1.5)) +
+  geom_point(data = outlier, aes(x = displ, y = hwy, size = 2, fill = "red")) +
+  geom_text(data = outlier, aes(label = model), hjust = "left")
+
+
+
+
+
+
+
+
+
+
+
